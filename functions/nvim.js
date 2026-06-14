0626fc4f8d3bc5d8d07533820d1f721f35540d53
eeriemyxi <myxi@envs.net>
2026-06-14 19:55:28 +0530

    feat: add `nvim` function to fetch nvim config

--- functions/nvim.js
+++ functions/nvim.js
@@ -1,3 +1,20 @@
-export function onRequest(context) {
-  return new Response("Hello, world!");
+export async function onRequest(context) {
+  const rawUrl = 'https://raw.githubusercontent.com/eeriemyxi/dotfiles/refs/heads/main/.config/nvim/init.lua';
+
+  try {
+    const response = await fetch(rawUrl);
+
+    if (!response.ok) {
+      return new Response(`Failed to fetch file. GitHub API status: ${response.status}`, { status: response.status });
+    }
+
+    const textContent = await response.text();
+    
+    return new Response(textContent, {
+      headers: { 'Content-Type': 'text/plain; charset=utf-8' }
+    });
+
+  } catch (error) {
+    return new Response(`Internal Server Error: ${error.message}`, { status: 500 });
+  }
 }
